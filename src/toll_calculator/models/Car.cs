42f2a903048e98a7ad5d9e2f7a5082f17b5a651f
RobinAxelsson <axelsson.robin@outlook.com>
2023-01-16 08:21:34 +0100

    chore: cleaned using statements

--- src/toll_calculator/models/Car.cs
+++ src/toll_calculator/models/Car.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace toll_calculator.models
+﻿namespace toll_calculator.models
 {
     public class Car : Vehicle
     {
