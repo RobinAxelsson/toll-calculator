e39ea8a84eb55ceda00362882ef7b297f80cabf7
RobinAxelsson <axelsson.robin@outlook.com>
2024-03-07 15:06:20 +0100

    refac: default constructor syntax

--- src/TrafficToll/TollCalculator.cs
+++ src/TrafficToll/TollCalculator.cs
@@ -7,18 +7,28 @@ namespace TrafficToll
 {
     public class TollCalculator
     {
-        private TollCalculatorCore? _tollCalculatorCore;
-        private TollPassingVerifyer? _passingVerifyer;
-        private TollPassingVerifyer TollPassingVerifyer => _passingVerifyer 
-            ??= new TollPassingVerifyer(TrafficTollDataManager.GetTollFreeParameters());
-        private TollCalculatorCore TollCalculatorCore => _tollCalculatorCore 
-            ??= new TollCalculatorCore(TrafficTollDataManager.GetTollCalculationParameters());
+        private TollCalculatorCore _tollCalculatorCore;
+        private TollPassingVerifyer _tollPassingVerifyer;
+
+        public TollCalculator() : this(new TollPassingVerifyer(TrafficTollDataManager.GetTollFreeParameters()), new TollCalculatorCore(TrafficTollDataManager.GetTollCalculationParameters())){}
+
+        private TollCalculator(TollPassingVerifyer tollPassingVerifyer, TollCalculatorCore tollCalculatorCore)
+        {
+            _tollPassingVerifyer = tollPassingVerifyer;
+            _tollCalculatorCore = tollCalculatorCore;
+        }
+
         public int GetTollFee(IEnumerable<DateTime> passings, int vehicleType)
         {
+            passings = passings.ToArray();
+            
             ValidatorCalculationArguments.EnsureArgumentsIsValid(passings, vehicleType);
-            var tollablePassings = TollPassingVerifyer.GetTollablePassings(passings, (VehicleType)vehicleType);
+            
+            var tollablePassings = _tollPassingVerifyer.GetTollablePassings(passings, (VehicleType)vehicleType).ToArray();
+            
             if (!tollablePassings.Any()) return 0;
-            return TollCalculatorCore.CalculateTollFee(tollablePassings);
+            
+            return _tollCalculatorCore.CalculateTollFee(tollablePassings);
         }
     }
 }
